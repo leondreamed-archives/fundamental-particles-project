c4e1001829fb70097bee33da9da52a51db32d07d
Leon Si <contact@leonzalion.com>
2022-01-06 20:46:43 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+	base: '/fundamental-particles-project',
 	resolve: {
 		alias: {
 			'~': path.resolve(new URL('.', import.meta.url).pathname, './src'),
