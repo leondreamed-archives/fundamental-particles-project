cded560aec4ded192c003759d5713152e8f74aae
Leon Si <contact@leonzalion.com>
2022-01-07 21:53:17 -0500

    add link to source code

--- src/pages/menu-page.vue
+++ src/pages/menu-page.vue
@@ -1,25 +1,39 @@
 <template>
-	<div class="column center h-full p-8">
-		<div class="text-[8rem] text-center font-bold leading-none">
-			Fundamental Particles Game
+	<div class="column items-center h-full p-8">
+		<div class="mb-auto flex-1"></div>
+		<div class="flex-1 column center">
+			<div class="text-[8rem] text-center font-bold leading-none">
+				Fundamental Particles Game
+			</div>
+			<div class="text-xl"><strong>By:</strong> Leon Si</div>
+			<div class="text-lg text-center my-5">
+				<span class="font-bold">Instructions: </span>
+				<span>
+					Drag the fundamental particles to the grid cell with the correct
+					information!
+				</span>
+			</div>
+			<VCheckbox v-model="store.hardMode" class="mb-5" input-class="h-8 w-8">
+				<span class="text-3xl font-bold">Hard Mode</span>
+			</VCheckbox>
+			<div
+				role="button"
+				class="bg-green-500 text-white font-bold rounded-md px-5 py-2 text-4xl hover:bg-green-600 cursor-pointer"
+				@click="startGame"
+			>
+				Start Game
+			</div>
 		</div>
-		<div class="text-xl"><strong>By:</strong> Leon Si</div>
-		<div class="text-lg text-center my-5">
-			<span class="font-bold">Instructions: </span>
-			<span>
-				Drag the fundamental particles to the grid cell with the correct
-				information!
-			</span>
-		</div>
-		<VCheckbox v-model="store.hardMode" class="mb-5" input-class="h-8 w-8">
-			<span class="text-3xl font-bold">Hard Mode</span>
-		</VCheckbox>
-		<div
-			role="button"
-			class="bg-green-500 text-white font-bold rounded-md px-5 py-2 text-4xl hover:bg-green-600 cursor-pointer"
-			@click="startGame"
-		>
-			Start Game
+		<div class="mt-auto flex-1 column justify-end">
+			<div>
+				<strong>Source Code: </strong>
+				<a
+					class="text-blue-500 hover:text-blue-600 underline"
+					target="_blank"
+					href="https://github.com/leonzalion/fundamental-particles-project"
+					>https://github.com/leonzalion/fundamental-particles-project
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
